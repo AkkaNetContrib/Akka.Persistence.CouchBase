cd4a613c525c3dbb68f8f42d0ed2d9a24978a828
ibrahim.hadad@coralfire.com <ibrahim.hadad@coralfire.com>
2016-02-17 21:59:27 -0500

    Added ability to use the ClusterHelper (if specified in the config).

--- src/Akka.Persistence.CouchBase/CouchBaseDBExtension.cs
+++ src/Akka.Persistence.CouchBase/CouchBaseDBExtension.cs
@@ -23,16 +23,16 @@ namespace Akka.Persistence.CouchBase
 
         public CouchBaseJournalSettings JournalSettings { get; private set; }
 
-        public Cluster JournalCBCluster{get; private set;}
+        public Couchbase.Core.ICluster JournalCBCluster{get; private set;}
 
-        public CouchbaseBucket JournalCBBucket{ get; private set;}
+        public Couchbase.Core.IBucket JournalCBBucket{ get; private set;}
 
 
         public CouchbaseSnapshotSettings SnapShotStoreSettings { get; private set; }
 
-        public Cluster SnapShotStoreCBCluster { get; private set; }
+        public Couchbase.Core.ICluster SnapShotStoreCBCluster { get; private set; }
 
-        public CouchbaseBucket SnapShotStoreCBBucket { get; private set; }
+        public Couchbase.Core.IBucket SnapShotStoreCBBucket { get; private set; }
 
 
         public CouchBaseDBExtension(ExtendedActorSystem system)
@@ -46,36 +46,31 @@ namespace Akka.Persistence.CouchBase
             system.Settings.InjectTopLevelFallback(CouchBaseDBPersistence.DefaultConfiguration());
 
             var HOCON_CB_JournalConfig = system.Settings.Config.GetConfig("akka.persistence.journal.couchbase");
+
             JournalSettings = new CouchBaseJournalSettings(HOCON_CB_JournalConfig);
 
-            // Instantiate the connection to the cluster
-            JournalCBCluster = new Cluster(JournalSettings.CBClientConfiguration);
+            if(JournalSettings.UseClusterHelper)
+            {
+                JournalCBBucket = ClusterHelper.GetBucket(JournalSettings.BucketName);
+                JournalCBCluster = JournalCBBucket.Cluster;
+            }
+            else
+            {
+                // Instantiate the connection to the cluster
+                JournalCBCluster = new Cluster(JournalSettings.CBClientConfiguration);
 
-            //Open the bucket and make a reference to the CB Client Configuration
-            JournalCBBucket = (CouchbaseBucket)JournalCBCluster.OpenBucket(JournalSettings.BucketName);
+                //Open the bucket and make a reference to the CB Client Configuration
+                JournalCBBucket = (CouchbaseBucket)JournalCBCluster.OpenBucket(JournalSettings.BucketName);
+            }
 
             // Throw an exception if we reach this point without a CB Cluster, CB Config, or Bucket
             if (JournalCBCluster == null)
                 throw new Exception("CouchBase Journal Cluster could not initialized.");
+
+            // Had to do it this way since by default CB always initializes the default bucket.
             if (JournalCBBucket.Name!=JournalSettings.BucketName)
             {
-                // This was a bad idea.  Creating a bucket on the fly takes tooooooo long.
-                // Plus the Cluster Provisioner has the user name and password parameters out of order.
-                // Like Donald Trump says "Bad!!!!"
-                //if(JournalSettings.AdminUserName != null && JournalSettings.AdminPassword != null)
-                //{
-                //    // Instantiate the bucket
-                //    var provisioner = new ClusterProvisioner(JournalCBCluster,JournalSettings.AdminUserName,JournalSettings.AdminPassword);
-                //    provisioner.ProvisionBucketAsync(new BucketSettings{
-                //        Name = JournalSettings.BucketName,
-                //        SaslPassword = JournalSettings.CBClientConfiguration.BucketConfigs[JournalSettings.BucketName].Password,
-                //        BucketType = Couchbase.Core.Buckets.BucketTypeEnum.Couchbase,
-                //    }).Wait();
-                //}
-                //else
-                //{
-                    throw new Exception("CouchBase Journal bucket could not initialized.");
-                //}
+                throw new Exception("CouchBase Journal bucket could not initialized.");
             }
 
 
@@ -112,48 +107,41 @@ namespace Akka.Persistence.CouchBase
             var HOCON_CB_SnapshotConfig = system.Settings.Config.GetConfig("akka.persistence.snapshot-store.couchbase");
             SnapShotStoreSettings = new CouchbaseSnapshotSettings(HOCON_CB_SnapshotConfig);
 
-            // Are we using the same cluster as the journal?
-            if( SnapShotStoreSettings.CBClientConfiguration.Servers.All(JournalSettings.CBClientConfiguration.Servers.Contains))
+            if(SnapShotStoreSettings.UseClusterHelper)
             {
-                SnapShotStoreCBCluster = JournalCBCluster;
-                
-                // Since we are using the same cluster are we using the same bucket?
-                if(SnapShotStoreSettings.BucketName == JournalSettings.BucketName)
+                SnapShotStoreCBBucket = ClusterHelper.GetBucket(SnapShotStoreSettings.BucketName);
+                SnapShotStoreCBCluster = SnapShotStoreCBBucket.Cluster;
+            }
+            else
+            {
+                // Are we using the same cluster as the journal?
+                if (SnapShotStoreSettings.CBClientConfiguration.Servers.All(JournalSettings.CBClientConfiguration.Servers.Contains))
                 {
-                    SnapShotStoreCBBucket = JournalCBBucket;
+                    SnapShotStoreCBCluster = JournalCBCluster;
+
+                    // Since we are using the same cluster are we using the same bucket?
+                    if (SnapShotStoreSettings.BucketName == JournalSettings.BucketName)
+                    {
+                        SnapShotStoreCBBucket = JournalCBBucket;
+                    }
+
                 }
+                else // Instantiate the connection to the new cluster
+                {
+                    SnapShotStoreCBCluster = new Cluster(SnapShotStoreSettings.CBClientConfiguration);
 
+                    //Open the bucket and make a reference to the CB Client Configuration
+                    SnapShotStoreCBBucket = (CouchbaseBucket)JournalCBCluster.OpenBucket(SnapShotStoreSettings.BucketName);
+                }
             }
-            else // Instantiate the connection to the new cluster
-            {
-                SnapShotStoreCBCluster = new Cluster(SnapShotStoreSettings.CBClientConfiguration);
 
-                //Open the bucket and make a reference to the CB Client Configuration
-                SnapShotStoreCBBucket = (CouchbaseBucket)JournalCBCluster.OpenBucket(SnapShotStoreSettings.BucketName);
-            }
-
-              // Throw an exception if we reach this point without a CB Cluster, CB Config, or Bucket
+            // Throw an exception if we reach this point without a CB Cluster, CB Config, or Bucket
             if (SnapShotStoreCBCluster == null)
                 throw new Exception("CouchBase Snapshot Store Cluster could not initialized.");
+
             if (SnapShotStoreCBBucket == null)
             {
-                // This was a bad idea.  Creating a bucket on the fly takes tooooooo long.
-                // Plus the Cluster Provisioner has the user name and password parameters out of order.
-                // Like Donald Trump says "Bad!!!!"
-                //if(SnapShotStoreSettings.AdminUserName != null && SnapShotStoreSettings.AdminPassword != null)
-                //{
-                //    // Instantiate the bucket
-                //    var provisioner = new ClusterProvisioner(JournalCBCluster,SnapShotStoreSettings.AdminPassword,SnapShotStoreSettings.AdminUserName);
-                //    provisioner.ProvisionBucketAsync(new BucketSettings{
-                //        Name = SnapShotStoreSettings.BucketName,
-                //        SaslPassword = SnapShotStoreSettings.CBClientConfiguration.BucketConfigs[SnapShotStoreSettings.BucketName].Password,
-                //        BucketType = Couchbase.Core.Buckets.BucketTypeEnum.Couchbase,
-                //    }).Wait();
-                //}
-                //else
-                //{
-                    throw new Exception("CouchBase Snapshot Store bucket could not initialized.");
-                //}
+                throw new Exception("CouchBase Snapshot Store bucket could not initialized.");
             }
 
             // Add Snapshot indexes
--- src/Akka.Persistence.CouchBase/CouchDBSettings.cs
+++ src/Akka.Persistence.CouchBase/CouchDBSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Akka.Configuration;
+using Couchbase;
 using Couchbase.Configuration.Client;
 using Couchbase.Core.Serialization;
 using Newtonsoft.Json;
@@ -11,14 +12,9 @@ namespace Akka.Persistence.CouchBase
     {
 
         public ClientConfiguration CBClientConfiguration { get; private set; }
+
         public string BucketName { get; private set; }
-
-        // Not needed since we are not generating a bucket on the fly.
-        //public string AdminUserName { get; private set; }
-
-        //public string AdminPassword { get; private set; }
-
-
+        public bool UseClusterHelper { get; private set; }
 
 
         public CouchDBSettings()
@@ -27,61 +23,66 @@ namespace Akka.Persistence.CouchBase
 
         protected void CreateCouchBaseDBClientConfiguration(Config config)
         {
-            CBClientConfiguration = new ClientConfiguration();
-
-            // Reset the serializers and deserializers so that JSON is stored as PascalCase instead of camelCase
-            CBClientConfiguration.Serializer = () => new DefaultSerializer(new JsonSerializerSettings(), new JsonSerializerSettings());
-            CBClientConfiguration.Servers.RemoveAt(0);
-
-            //Get the URI's from the HOCON config
-            try
+            if(config.GetBoolean("UseClusterHelper"))
             {
-                if (config.GetStringList("ServersURI").Count > 0)
+                BucketName = config.GetString("BucketName");
+                CBClientConfiguration = ClusterHelper.GetBucket(BucketName).Cluster.Configuration;
+                UseClusterHelper = true;
+            }
+            else
+            {
+                CBClientConfiguration = new ClientConfiguration();
+
+                // Reset the serializers and deserializers so that JSON is stored as PascalCase instead of camelCase
+                CBClientConfiguration.Serializer = () => new DefaultSerializer(new JsonSerializerSettings(), new JsonSerializerSettings());
+                CBClientConfiguration.Servers.RemoveAt(0);
+
+                //Get the URI's from the HOCON config
+                try
                 {
-                    List<Uri> uris = new List<Uri>();
-                    foreach (string s in config.GetStringList("ServersURI"))
+                    if (config.GetStringList("ServersURI").Count > 0)
                     {
-                        CBClientConfiguration.Servers.Add(new Uri(s));
+                        List<Uri> uris = new List<Uri>();
+                        foreach (string s in config.GetStringList("ServersURI"))
+                        {
+                            CBClientConfiguration.Servers.Add(new Uri(s));
+                        }
                     }
+
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Invalid URI specified in HOCON configuration", ex);
                 }
 
+                // Use SSL?
+                CBClientConfiguration.UseSsl = config.GetBoolean("UseSSL");
+
+                // This will not be needed since we are not creating a bucket on the fly.
+                //AdminPassword = config.GetString("AdminPassword");
+                //AdminUserName = config.GetString("AdminUserName");
+
+
+                // Get the bucket(s) configuration
+                Dictionary<string, BucketConfiguration> BucketConfigs = new Dictionary<string, BucketConfiguration>();
+                BucketConfiguration newBC = new BucketConfiguration();
+
+
+                newBC.UseSsl = config.GetBoolean("BucketUseSSL");
+                newBC.Password = config.GetString("Password");
+                newBC.DefaultOperationLifespan = (uint)config.GetInt("DefaultOperationLifespan");
+                BucketName = config.GetString("BucketName");
+                newBC.BucketName = BucketName;
+                newBC.PoolConfiguration.MinSize = config.GetInt("PoolConfiguration.MinSize");
+                newBC.PoolConfiguration.MaxSize = config.GetInt("PoolConfiguration.MaxSize");
+
+                // Create the bucket config specified in the HOCON
+                BucketConfigs.Add(newBC.BucketName, newBC);
+                CBClientConfiguration.BucketConfigs = BucketConfigs;
 
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Invalid URI specified in HOCON configuration", ex);
-            }
-
-            // Use SSL?
-            CBClientConfiguration.UseSsl = config.GetBoolean("UseSSL");
-
-            // This will not be needed since we are not creating a bucket on the fly.
-            //AdminPassword = config.GetString("AdminPassword");
-            //AdminUserName = config.GetString("AdminUserName");
-
-
-            // Get the bucket(s) configuration
-            Dictionary<string, BucketConfiguration> BucketConfigs = new Dictionary<string, BucketConfiguration>();
-            BucketConfiguration newBC = new BucketConfiguration();
-
-
-            newBC.UseSsl = config.GetBoolean("BucketUseSSL");
-            newBC.Password = config.GetString("Password");
-            newBC.DefaultOperationLifespan = (uint)config.GetInt("DefaultOperationLifespan");
-            BucketName = config.GetString("BucketName");
-            newBC.BucketName = BucketName;
-            newBC.PoolConfiguration.MinSize = config.GetInt("PoolConfiguration.MinSize");
-            newBC.PoolConfiguration.MaxSize = config.GetInt("PoolConfiguration.MaxSize");
-
-            // Create the bucket config specified in the HOCON
-            BucketConfigs.Add(newBC.BucketName, newBC);
-            CBClientConfiguration.BucketConfigs = BucketConfigs;
-
-
-
         }
-
-
     }
 
     public class CouchBaseJournalSettings : CouchDBSettings
